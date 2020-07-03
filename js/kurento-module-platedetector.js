5549f475cccbfbd1619542326b9f9cb5d343d159
Jenkins Kurento <jenkins@kurento.org>
2020-07-03 18:47:08 +0000

    Code autogenerated from Kurento/kurento-module-platedetector-js@63e9d18

--- js/kurento-module-platedetector.js
+++ js/kurento-module-platedetector.js
@@ -177,7 +177,7 @@ require('.');
  */
 
 Object.defineProperty(exports, 'name',    {value: 'platedetector'});
-Object.defineProperty(exports, 'version', {value: '6.7.1-dev'});
+Object.defineProperty(exports, 'version', {value: '6.14.1-dev'});
 
 
 var PlateDetectorFilter = require('./PlateDetectorFilter');
